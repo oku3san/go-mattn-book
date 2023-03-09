9df7ab94cfca2818421449be4a736cb02896707d
oku3san <takaya0303@gmail.com>
2023-03-10 00:45:35 +0900

    Update

--- main.go
+++ main.go
@@ -2,21 +2,6 @@ package main
 
 import "fmt"
 
-//type Fruit int
-//type Animal int
-//
-//const (
-//    Apple Fruit = iota
-//    Orange
-//    Banana
-//)
-//
-//const (
-//    Monkey Animal = iota
-//    Elephant
-//    Pig
-//)
-
 type User struct {
     Name string
     Age  int
