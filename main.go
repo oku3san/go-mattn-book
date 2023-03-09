d081c47a4843ece75f0b48e84e91aaeeeeca8561
oku3san <takaya0303@gmail.com>
2023-03-10 00:45:27 +0900

    Update

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //type Fruit int
 //type Animal int
 //
@@ -15,50 +17,20 @@ package main
 //    Pig
 //)
 
+type User struct {
+    Name string
+    Age  int
+}
+
 func main() {
-    //var fruit Fruit = Apple
-    //fmt.Println(fruit)
-    //
-    //fruit = Elephant
-    //fmt.Println(fruit)
+    user := User{
+        Name: "Bob",
+        Age:  18,
+    }
+    showName(&user)
 
-    //a := []int{1, 2, 3}
-    //fmt.Println(a)
-    //a = append(a, 4, 5, 6)
-    //fmt.Println(a, len(a))
-    //
-    //fmt.Println(a[2:5])
+}
 
-    //s := "こんにちわ世界"
-    //rs := []rune(s)
-    //rs[4] = 'は'
-    //s = string(rs)
-    //println(s)
-
-    //m := make(map[string]int)
-    //m["John"] = 21
-    //m["Bob"] = 18
-    //m["Mark"] = 33
-    //
-    //keys := []string{}
-    //for k := range m {
-    //    fmt.Println(k)
-    //    keys = append(keys, k)
-    //}
-    //fmt.Println(keys)
-    //sort.Strings(keys)
-    //fmt.Println(keys)
-    //for _, k := range keys {
-    //    fmt.Printf("%v, %v\n", k, m[k])
-    //}
-
-    //m := map[string]string{
-    //    "foo": "bar",
-    //    "zoo": "zoo",
-    //}
-    //v, ok := m["zoo"]
-    //fmt.Println(ok)
-    //if ok {
-    //    fmt.Println(v)
-    //}
+func showName(user *User) {
+    fmt.Println(user.Name)
 }
