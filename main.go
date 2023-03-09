3c5bd5a19ef9645cb4214c82369ea9e240f4ed3b
oku3san <takaya0303@gmail.com>
2023-03-10 00:20:34 +0900

    Update

--- main.go
+++ main.go
@@ -21,4 +21,17 @@ func main() {
     //
     //fruit = Elephant
     //fmt.Println(fruit)
+
+    //a := []int{1, 2, 3}
+    //fmt.Println(a)
+    //a = append(a, 4, 5, 6)
+    //fmt.Println(a, len(a))
+    //
+    //fmt.Println(a[2:5])
+
+    //s := "こんにちわ世界"
+    //rs := []rune(s)
+    //rs[4] = 'は'
+    //s = string(rs)
+    //println(s)
 }
