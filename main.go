241c204004b02ad10c7ca52302783037a780d823
oku3san <takaya0303@gmail.com>
2023-03-11 19:19:01 +0900

    Update

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    defer fmt.Println("6")
-    defer fmt.Println("5")
-    defer fmt.Println("4")
-    fmt.Println("1")
-    fmt.Println("2")
-    fmt.Println("3")
+    //defer fmt.Println("6")
+    //defer fmt.Println("5")
+    //defer fmt.Println("4")
+    //fmt.Println("1")
+    //fmt.Println("2")
+    //fmt.Println("3")
+    doSomething()
+}
+
+func doSomething() {
+    var n = 1
+    defer func() {
+        fmt.Println(n)
+    }()
+
+    n = 2
 }
