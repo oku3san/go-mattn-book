98774532978cf8f2321be977abc4525530840fae
oku3san <takaya0303@gmail.com>
2023-03-11 22:54:36 +0900

    Update

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
 func main() {
 
     message := "hi"
-    go sendMessage(message)
+    go func() {
+        sendMessage(message)
+    }()
+    message = "ho"
+
+    time.Sleep(time.Second)
+    fmt.Println(message)
+    time.Sleep(time.Second)
 }
 
 func sendMessage(msg string) {
