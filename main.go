442ca0e4e7f669818f2b49487f8014eb3dc2f753
oku3san <takaya0303@gmail.com>
2023-03-11 17:01:33 +0900

    Update

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import "fmt"
 
 type Value int
 
-func (v Value) Add(n Value) Value {
-    return v + n
+func (v *Value) Add(n Value) {
+    //return v + n
+    *v += n
 }
 
 func main() {
     v := Value(1)
-    v = v.Add(2)
+    v.Add(2)
     fmt.Println(v)
+
+    //v := 1
+    //p := &v
+    //*p = 2
+    //fmt.Println(v)
 }
