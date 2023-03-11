107b999c47b173cae415a8db342677de549728aa
oku3san <takaya0303@gmail.com>
2023-03-11 19:26:48 +0900

    Update

--- main.go
+++ main.go
@@ -3,23 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    //defer fmt.Println("6")
-    //defer fmt.Println("5")
-    //defer fmt.Println("4")
-    //fmt.Println("1")
-    //fmt.Println("2")
-    //fmt.Println("3")
-    doSomething()
+
+    message := "hi"
+    go sendMessage(message)
 }
 
-func doSomething() {
-    //var n = 1
-    //defer func() {
-    //    fmt.Println(n)
-    //}()
-    //n = 2
-
-    var n = 1
-    defer fmt.Println(n)
-    n = 2
+func sendMessage(msg string) {
+    fmt.Println(msg)
 }
