1409617c55c3cb09f25e85d608ea42b942ec1333
oku3san <takaya0303@gmail.com>
2023-03-11 23:13:51 +0900

    Update

--- main.go
+++ main.go
@@ -8,26 +8,20 @@ import (
 func main() {
     n := 0
 
-    var mu sync.Mutex
-
     var wg sync.WaitGroup
     wg.Add(2)
 
     go func() {
         defer wg.Done()
         for i := 0; i < 1000; i++ {
-            mu.Lock()
             n++
-            mu.Unlock()
         }
     }()
 
     go func() {
         defer wg.Done()
         for i := 0; i < 1000; i++ {
-            mu.Lock()
             n++
-            mu.Unlock()
         }
     }()
 
