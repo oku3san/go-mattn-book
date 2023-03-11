f0b791a36540672db79a8e1efd263bb5df4123fb
oku3san <takaya0303@gmail.com>
2023-03-11 17:06:50 +0900

    Update

--- main.go
+++ main.go
@@ -2,34 +2,11 @@ package main
 
 import "fmt"
 
-type Speaker interface {
-    Speak() error
-}
-
-type Dog struct {
-}
-
-func (d *Dog) Speak() error {
-    fmt.Println("BowWow")
-    return nil
-}
-
-type Cat struct {
-}
-
-func (c *Cat) Speak() error {
-    fmt.Println("Meow")
-    return nil
-}
-
-func DoSpeak(s Speaker) error {
-    return s.Speak()
-}
-
 func main() {
-    dog := Dog{}
-    DoSpeak(&dog)
-
-    cat := Cat{}
-    DoSpeak(&cat)
+    defer fmt.Println("6")
+    defer fmt.Println("5")
+    defer fmt.Println("4")
+    fmt.Println("1")
+    fmt.Println("2")
+    fmt.Println("3")
 }
